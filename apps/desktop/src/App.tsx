c8e3f49463ca5272bf4020ecb3c8e23b131489fb
Sarib Mahmood <96064700+saribmah@users.noreply.github.com>
2026-02-06 00:30:06 -0500

    fix routes

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -1,10 +1,4 @@
 import "./App.css";
-import {
-  OnboardingDemoView,
-  ResponsesDemoView,
-  SettingsDemoView,
-  UiKitView,
-} from "@cocommand/demo";
 import { CommandView } from "./features/command/command.view";
 import { SettingsView } from "./features/settings/settings.view";
 import "@cocommand/ui";
@@ -16,20 +10,9 @@ function App() {
   return (
     <AppContainer>
       <AppInit>
-        <Routes>
-          <Route path="/settings" element={<SettingsView />} />
-          <Route path="/ui-kit" element={<UiKitView />} />
-          <Route path="/ui-onboarding" element={<OnboardingDemoView />} />
-          <Route path="/ui-settings" element={<SettingsDemoView />} />
-          <Route path="/ui-responses" element={<ResponsesDemoView />} />
-          <Route
-            path="/"
-            element={
-              <main className="container">
-                <CommandView />
-              </main>
-            }
-          />
+          <Routes>
+              <Route path="/settings" element={<SettingsView />} />
+              <Route path="/" element={<CommandView />} />
         </Routes>
       </AppInit>
     </AppContainer>
